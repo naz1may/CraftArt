506f4ed785fe8165db41b3b5ba939646a7646d2b
Nazimay <nazimajkadyrbekova@gmail.com>
2025-02-15 13:31:13 +0600

    home page html

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+// $(document).ready(function(){
+//     console.log('Документ загружен!');
+    
+//     let position = 0;
+//     const slidesToShow = 1;
+//     const slidesToScroll = 1;
+//     const container = $('.slider-container');
+//     const track = $('.slider-track');
+//     const item = $('.slider-item');
+//     const btnPrev = $('.btn-prev');
+//     const btnNext = $('.btn-next');
+//     const itemWidth = container.width() / slidesToShow;
+
+//     item.each(function (index, item){
+//         $(item).css({
+//             minWidth: itemWidth,
+//         })
+//     });
+
+//     btnPrev.click(function(){
+//         console.log('btnPrev');
+//     })
+//     btnNext.click(function(){
+//         console.log('btnNext');
+//     })
+// })
